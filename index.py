dea4f506da49e04f975e567adca451d873ed07e5
Sadman Hasan <s4dman@users.noreply.github.com>
2020-03-14 00:46:30 -0400

    Updated mechanize browser variable

--- index.py
+++ index.py
@@ -3,31 +3,37 @@ from bs4 import BeautifulSoup
 
 
 def to_scrape():
-    br = mechanize.Browser()
+    
+    browser = mechanize.Browser()
+    
     # Ignore robots.txt.  Do not do this without thought and consideration
-    br.set_handle_robots(False)
+    browser.set_handle_robots(False)
+    
     # Pass a browser agent to show that the request is coming form a browser
-    br.addheaders = [
+    browser.addheaders = [
         ('User-agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:18.0)Gecko/20100101 Firefox/18.0 (compatible;)'),
         ('Accept', '*/*')]
-    # Browsing the home
-    br.open("http://quotes.toscrape.com/")
+    
+    # Browsing the home page
+    browser.open("http://quotes.toscrape.com/")
+    
     # Finding the login link
-    for login_link in br.links(url_regex="/login"):
-        br.open("http://quotes.toscrape.com" + login_link.url)
+    for login_link in browser.links(url_regex="/login"):
+        browser.open("http://quotes.toscrape.com" + login_link.url)
         # Authorization Starts
-        br.select_form(nr=0)  # Selecting the login form
-        br.form['username'] = 'King'  # Setting username for the input field
-        br.form['password'] = 'password'  # Setting password for the input field
-        br.submit()
-        print (br.geturl())  # On submit getting the redirected URL
+        browser.select_form(nr=0)  # Selecting the login form
+        browser.form['username'] = 'King'  # Setting username for the input field
+        browser.form['password'] = 'password'  # Setting password for the input field
+        browser.submit()
+        print (browser.geturl())  # On submit getting the redirected URL
 
-        html = br.open(br.geturl())
+        html = browser.open(browser.geturl())
+        
         # If logged in, there should have a logout link available
-        for logout_link in br.links(url_regex="/logout"):
+        for logout_link in browser.links(url_regex="/logout"):
             print ("http://quotes.toscrape.com" + logout_link.url)
 
-        soup = BeautifulSoup(html, "html.parser")  # Converting to BeautifulSoup object
+        soup = BeautifulSoup(html, "html.parser")  # Converting webpage to BeautifulSoup object
         for parent_div in soup.findAll('div', {'class': 'quote'}):
             quote = parent_div.span.text
             author = parent_div.small.text
